d2a1fdf53799d48c15c12b31ccfcbcabf8227433
drdexe <104161856+drdexe@users.noreply.github.com>
2025-04-25 20:18:55 +0800

    Add operator functions

---
+++ script.js
@@ -0,0 +1,17 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
+}
